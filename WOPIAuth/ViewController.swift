c76b2d4f5ad5f2930a73de7f65e0fc941c2febcb
Gary Ritchie <gary.t.ritchie@gmail.com>
2016-04-07 20:15:18 -0700

    Initial Commit

---
+++ WOPIAuth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WOPIAuth
+//
+//  Created by Gary Ritchie on 4/7/16.
+//  Copyright © 2016 Gary Ritchie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
