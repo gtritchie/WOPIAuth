7ec0ed7a7ed1c5e477d7e1dd5dc211d60d6ed288
Gary Ritchie <gary.t.ritchie@gmail.com>
2016-04-22 14:00:46 -0700

    don't create the date time formatter on every call

--- LogViewController.swift
+++ LogViewController.swift
@@ -7,6 +7,8 @@ class LogViewController: NSViewController {
 	
 	dynamic var log: NSAttributedString = NSAttributedString(string: "")
 	
+	private static var RFC3339DateFormatter: NSDateFormatter?
+
 	// MARK: Outlets
 	
 	@IBOutlet var textView: NSTextView!
@@ -28,11 +30,9 @@ class LogViewController: NSViewController {
 		If the error attribute was set, make the text red.
 	*/
 	func didReceiveLogLineNotification(note: NSNotification) {
-		let mutableLog = log.mutableCopy() as! NSMutableAttributedString
-		if log.length > 0 {
-			mutableLog.appendAttributedString(NSAttributedString(string: "\n"))
-		}
 
+		// Extract string and optional error flag from the notification and build
+		// our formatted logging string
 		let userInfo = note.userInfo! as! [String: String]
 		let message = userInfo[LogLineNotificationMessageKey]!
 
@@ -45,26 +45,35 @@ class LogViewController: NSViewController {
 			messageFormat = NSLocalizedString("%1$@: %2$@", comment: "Date: message")
 		}
 
-		let now = NSDate()
-		let RFC3339DateFormatter = NSDateFormatter()
-		RFC3339DateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-		RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-		RFC3339DateFormatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
-		let logTime = RFC3339DateFormatter.stringFromDate(now)
-		
-		let fullPlainLine = String(format: messageFormat, logTime, message)
-
-		let logLine = NSMutableAttributedString(string: fullPlainLine)
+		let logLine = NSMutableAttributedString(string: String(format: messageFormat, LogViewController.getCurrentTime(), message))
 		if isError {
 			logLine.addAttribute(NSForegroundColorAttributeName,
 			                     value: NSColor.redColor(),
 			                     range: NSRange(0..<logLine.length))
 		}
 
+		// Add the string and scroll the textview to show it
+		let mutableLog = log.mutableCopy() as! NSMutableAttributedString
+		if log.length > 0 {
+			mutableLog.appendAttributedString(NSAttributedString(string: "\n"))
+		}
 		mutableLog.appendAttributedString(logLine)
 		log = mutableLog.copy() as! NSAttributedString
 		
 		textView.scrollRangeToVisible(NSRange(location: log.length, length: 0))
+	}
+	
+	// MARK: Helpers
+	
+	/// Return current time as an RFC3339 formatted string
+	private static func getCurrentTime() -> String {
+		if RFC3339DateFormatter == nil {
+			RFC3339DateFormatter = NSDateFormatter()
+			RFC3339DateFormatter!.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+			RFC3339DateFormatter!.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+			RFC3339DateFormatter!.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+		}
+		return RFC3339DateFormatter!.stringFromDate(NSDate())
 	}
 }
 
