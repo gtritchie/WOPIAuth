81cfe3767eb013f43fc2a69c63edafae2c2adaf6
Gary Ritchie <gary.t.ritchie@gmail.com>
2016-04-24 10:45:23 -0700

    placeholder text in provider form and commented out validator functions

--- ProviderInfo.swift
+++ ProviderInfo.swift
@@ -100,4 +100,66 @@ private let currentProviderInfoVersion = 1
 		aCoder.encodeObject(self.clientSecret, forKey: clientSecretKey)
 		aCoder.encodeObject(self.redirectUrl, forKey: redirectUrlKey)
 	}
+	
+	// MARK: Validation
+	
+//	func validateProviderName(providerNameStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let providerName = providerNameStringPtr.memory
+//		if providerName == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "Provider Name cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
+//	
+//	func validateProviderID(providerIDStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let providerID = providerIDStringPtr.memory
+//		if providerID == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "ProviderID cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
+//	
+//	func validateBootstrapper(bootstrapperStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let bootstrapper = bootstrapperStringPtr.memory
+//		if bootstrapper == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "Bootstrapper URL cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
+//	
+//	func validateClientID(clientIDStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let clientID = clientIDStringPtr.memory
+//		if clientID == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "OAuth2 ClientID cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
+//	
+//	func validateClientSecret(clientSecretStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let clientSecret = clientSecretStringPtr.memory
+//		if clientSecret == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "OAuth2 Client Secret cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
+//	
+//	func validateRedirectURL(redirectURLStringPtr: AutoreleasingUnsafeMutablePointer<NSString?>) throws {
+//		let redirectURL = redirectURLStringPtr.memory
+//		if redirectURL == nil {
+//			let domain = "UserInputValidationErrorDomain"
+//			let code = 0
+//			let userInfo = [NSLocalizedDescriptionKey : "Redirect URL cannot be empty."]
+//			throw NSError(domain: domain, code: code, userInfo: userInfo)
+//		}
+//	}
 }
