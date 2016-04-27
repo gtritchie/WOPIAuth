30221df11aca32470a48212067f80ae91a86f61f
Gary Ritchie <gary.t.ritchie@gmail.com>
2016-04-27 13:44:47 -0700

    create all steps, and success failure checks with image settings and logging

--- WOPIFlowViewController.swift
+++ WOPIFlowViewController.swift
@@ -23,54 +23,136 @@ class WOPIFlowViewController: NSViewController, ConnectionCreating {
 	
 	@IBOutlet weak var bootstrapImage: NSImageView!
 	@IBOutlet weak var bootstrapText: NSTextField!
+	@IBOutlet weak var bootstrapProgress: NSProgressIndicator!
 	@IBOutlet weak var signinImage: NSImageView!
 	@IBOutlet weak var signinText: NSTextField!
+	@IBOutlet weak var signinProgress: NSProgressIndicator!
 	@IBOutlet weak var tokenImage: NSImageView!
 	@IBOutlet weak var tokenText: NSTextField!
+	@IBOutlet weak var tokenProgress: NSProgressIndicator!
 	@IBOutlet weak var profileImage: NSImageView!
 	@IBOutlet weak var profileText: NSTextField!
+	@IBOutlet weak var profileProgress: NSProgressIndicator!
 
+	// MARK: Track current step
+	
+	var currentStep: String?
 	weak var currentImage: NSImageView?
 	weak var currentTextLabel: NSTextField?
+	weak var currentProgress: NSProgressIndicator?
 	
 	// MARK: Life Cycle
 	
-	/// Set initial state before we show the view
 	override func viewWillAppear() {
 		bootstrapImage.image = nil
 		signinImage.image = nil
 		tokenImage.image = nil
 		profileImage.image = nil
-		
-		currentImage = bootstrapImage
-		currentTextLabel = bootstrapText
 	}
 	
-	// View is visible so start the flow
 	override func viewDidAppear() {
 		super.viewWillAppear()
 		
 		assert(provider != nil, "Must supply a provider to WOPIFlowViewController")
 		assert(connections != nil, "Must supply array of connections to WOPIFlowViewController ")
 	
-		WOPIAuthLogInfo("Starting WOPI client authentication flow")
-		
-		guard provider!.validate() == true else {
-			failCurrentStep()
-			return
-		}
-		
+		performAuthFlow()
 	}
 
 	func failCurrentStep() {
 		currentImage!.image = NSImage(named: "ErrorXCircle")
 		currentTextLabel!.textColor = NSColor.redColor()
-		WOPIAuthLogInfo("Stopping WOPI client authentication flow")
+		currentProgress!.stopAnimation(nil)
+		WOPIAuthLogError("FAILURE: WOPI client authentication flow during \(currentStep!) phase.")
+		WOPIAuthLogError("======================================================================")
 	}
+	
+	func completeCurrentStep() {
+		if let previousStep = currentStep {
+			WOPIAuthLogInfo("Completed step \(previousStep)")
+		}
+		if let previousImage = currentImage {
+			previousImage.image = NSImage(named: "SuccessCheckCircle")
+		}
+		if let previousProgress = currentProgress {
+			previousProgress.stopAnimation(nil)
+		}
+	}
+	
+	func startNewStep(step: String, image: NSImageView, text: NSTextField, progress: NSProgressIndicator) {
+		completeCurrentStep()
+		
+		currentStep = step
+		WOPIAuthLogInfo("Starting \(currentStep!)")
+		
+		currentImage = image
+		currentTextLabel = text
+		
+		currentProgress = progress
+		currentProgress!.startAnimation(nil)
+	}
+	
+	func performAuthFlow() {
+		
+		WOPIAuthLogInfo("START WOPI client authentication flow")
+		WOPIAuthLogInfo("=====================================")
+		
+		guard initialBootstrapperCall() else {
+			return
+		}
+		guard signIn() else {
+			return
+		}
+		guard getTokens() else {
+			return
+		}
+		guard getProfile() else {
+			return
+		}
+		
+		completeCurrentStep()
+		
+		WOPIAuthLogInfo("SUCCESS WOPI client authentication flow")
+		WOPIAuthLogInfo("=======================================")
+	}
+	
+	/// Step One: Unauthenticated Bootstrapper call
+	func initialBootstrapperCall() -> Bool {
+		startNewStep("bootstrapper", image: bootstrapImage, text: bootstrapText, progress: bootstrapProgress)
+
+		// Sanity check on ProviderInfo
+		guard provider!.validate() == true else {
+			failCurrentStep()
+			return false
+		}
+		
+		return true
+	}
+	
+	/// Step Two: Interactive Sign-In UI
+	func signIn() -> Bool {
+		startNewStep("signin", image: signinImage, text: signinText, progress: signinProgress)
+
+		return true
+	}
+	
+	/// Step Three: Obtain tokens
+	func getTokens() -> Bool {
+		startNewStep("tokens", image: tokenImage, text: tokenText, progress: tokenProgress)
+
+		return true
+	}
+	
+	// Step Four: Authenticated call to bootstrapper for user profile info
+	func getProfile() -> Bool {
+		startNewStep("profile", image: profileImage, text: profileText, progress: profileProgress)
+
+		return true
+	}
+	
 }
 
 
-//progressIndicator.startAnimation(sender)
 //let fetcher = BootstrapFetcher()
 //
 //fetcher.fetchBootstrapInfoUsingCompletionHandler { (result) in
